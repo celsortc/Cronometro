5f247380eec872fe02ba36e09fef7839ef85f5d0
Celso <celsortc@outlook.com>
2025-11-24 21:36:12 -0300

    lógica do cronometro funcionando, e botão pause adicionado

--- script.js
+++ script.js
@@ -6,34 +6,68 @@ const segundos = document.querySelector(".segundos");
 const minutos = document.querySelector(".minutos");
 
 var interval;
+var cronPausado = false;
 
 iniciar.onclick = () => {
-  var mili = +milisegundos.innerHTML;
-  segundos.innerHTML = 55;
+  if (cronPausado === true) {
+    var mili = +formataNumero(milisegundos.innerHTML);
+    var sec = +formataNumero(segundos.innerHTML);
+  } else {
+    var mili = +milisegundos.innerHTML;
+    var sec = +segundos.innerHTML;
+  }
+
+  // segundos.innerHTML = 55;
+  btnToggle();
   interval = setInterval(() => {
+    cronPausado = false;
     if (mili < 990) {
+      console.log("entrou 1", mili, sec, milisegundos.innerHTML);
+
       mili += 10;
       milisegundos.innerHTML = formataNumero(mili);
     } else if (mili === 990) {
+      console.log("entrou2", mili, sec, milisegundos.innerHTML);
+
       mili = 0;
       var sec = +segundos.innerHTML;
       sec++;
       segundos.innerHTML = formataNumero(sec);
-      console.log(typeof sec);
-    } else if (mili === 990 && sec === 59) {
-      segundos.innerHTML = "00";
-
-      var min = +minutos.innerHTML;
-      min++;
-      minutos.innerHTML = formataNumero(min);
+      if (sec > 59) {
+        sec = 0;
+        segundos.innerHTML = formataNumero(sec);
+        var min = +minutos.innerHTML;
+        min++;
+        minutos.innerHTML = formataNumero(min);
+      }
     }
-  }, 1);
+  }, 10);
 };
 
 function formataNumero(numero) {
-  if (numero < 100) {
-    return numero.toString().padStart(2, "0");
+  if (cronPausado === true) {
+    return numero.toString().padEnd(3, "0");
   } else {
-    return numero.toString().slice(0, 2);
+    if (numero < 100) {
+      return numero.toString().padStart(2, "0");
+    } else {
+      return numero.toString().slice(0, 2);
+    }
   }
 }
+
+function btnToggle() {
+  if (iniciar.disabled === true) {
+    iniciar.disabled = false;
+    pausar.disabled = true;
+  } else {
+    iniciar.disabled = true;
+    pausar.disabled = false;
+  }
+}
+
+pausar.onclick = () => {
+  clearInterval(interval);
+  btnToggle();
+  cronPausado = true;
+};
